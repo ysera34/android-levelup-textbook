559d5a05e0e4963c2355019593edf46846272665
ysera34 <ycyoon34@gmail.com>
2017-06-16 21:11:38 +0900

    fragmentManager onBackStackChangedListener

--- Chapter02/Fragment/app/src/main/java/com/android/sample/fragment/HomeActivity.java
+++ Chapter02/Fragment/app/src/main/java/com/android/sample/fragment/HomeActivity.java
@@ -2,8 +2,10 @@ package com.android.sample.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -12,7 +14,12 @@ import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String TAG = HomeActivity.class.getSimpleName();
+    private static final String FRAGMENT_TAG = "fragment_tag";
+    private static final String KEY_NUMBER = "key_number";
+
     private FragmentManager mFragmentManager;
+    private FragmentManager.OnBackStackChangedListener mBackStackChangedListener;
     private int mNumber;
 
     @Override
@@ -21,6 +28,21 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         mFragmentManager = getSupportFragmentManager();
+        mBackStackChangedListener = new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                int count = 0;
+                if (mFragmentManager.getFragments() != null) {
+                    for (Fragment fragment : mFragmentManager.getFragments()) {
+                        if (fragment != null) {
+                            count++;
+                        }
+                    }
+                    mNumber = count;
+                    Log.d(TAG, "onBackStackChanged mNumber: " + mNumber);
+                }
+            }
+        };
 
         findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +62,44 @@ public class HomeActivity extends AppCompatActivity {
                 mFragmentManager.popBackStack();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFragmentManager.addOnBackStackChangedListener(mBackStackChangedListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Fragment fragment = mFragmentManager.findFragmentByTag(FRAGMENT_TAG);
+        Log.d(TAG, "onCreate fragment: " + fragment + ", mNumber: " + mNumber);
+        if (fragment == null) {
+            mFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, HomeFragment.newInstance(mNumber), FRAGMENT_TAG)
+                    .addToBackStack(null)
+                    .commit();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mFragmentManager.removeOnBackStackChangedListener(mBackStackChangedListener);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_NUMBER, mNumber);
+        Log.d(TAG, "onSaveInstanceState: mNumber: " + mNumber);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mNumber = savedInstanceState.getInt(KEY_NUMBER);
+        Log.d(TAG, "onRestoreInstanceState: mNumber: " + mNumber);
     }
 }
