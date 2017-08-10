1a6a37d3a70858c4856256e2b27f76085cb9fb36
ysera34 <ycyoon34@gmail.com>
2017-08-10 12:37:11 +0900

    customize build type

--- Chapter06/Gradle/app/src/main/java/com/android/sample/gradle/MainActivity.java
+++ Chapter06/Gradle/app/src/main/java/com/android/sample/gradle/MainActivity.java
@@ -1,13 +1,20 @@
 package com.android.sample.gradle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.text_view);
+        mTextView.setText(getString(R.string.api_url));
     }
 }
